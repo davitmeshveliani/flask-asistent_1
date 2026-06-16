6caabd5cfcc0500bd0981cacf13031916d4deaba
davitmeshveliani <davitmeshveliani@gmail.com>
2026-06-16 15:24:29 +0200

    main.py, requirements.txt

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, davit!'
+
+@app.route('/user/<name>')
+def user(name):
+    return f'Hello, {name}!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
